cdce3492d30d093d5630252a64c8c823328d099f
Jhonny91360 <jhonnyzamsa@hotmail.com>
2025-04-07 22:04:41 -0500

    feat: animated xy

---
+++ src/presentation/screens/animations/Animation102Screen.tsx
@@ -0,0 +1,44 @@
+import React, {useRef} from 'react';
+import {Animated, PanResponder, StyleSheet, View} from 'react-native';
+
+export const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      {
+        dx: pan.x, // x,y are Animated.Value
+        dy: pan.y,
+      },
+    ]),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        {toValue: {x: 0, y: 0}, useNativeDriver: false}, // Back to zero
+      ).start();
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.purpleBox]}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  purpleBox: {
+    backgroundColor: '#5856D6',
+    width: 100,
+    height: 100,
+  },
+});
